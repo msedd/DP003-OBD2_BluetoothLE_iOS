bed8b4a2c060fbed0070ada4aa8ea4dd624e4735
Marko Seifert <marko.seifert@t-systems.com>
2015-09-30 20:23:10 +0200

    add cocoapods, reactive cocoa, crashlytics/fabric

--- iOS/ConnectMyCar/ConnectMyCar/AppDelegate.m
+++ iOS/ConnectMyCar/ConnectMyCar/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import <Fabric/Fabric.h>
+#import <Crashlytics/Crashlytics.h>
+
 
 @interface AppDelegate ()
 
@@ -16,6 +19,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    [Fabric with:@[[Crashlytics class]]];
+    [CrashlyticsKit crash];
+
+    
     // Override point for customization after application launch.
     return YES;
 }
